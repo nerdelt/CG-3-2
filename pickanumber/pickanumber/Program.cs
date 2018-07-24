b74649dec56f8b3b6d840e72a37a67d1b884c604
nerdelt <nerdelt@gmail.com>
2018-07-23 21:46:16 -0500

    Assignment 3-2

---
+++ pickanumber/pickanumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace pickanumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm thinking of a number between 1 and 10. Can you guess it?");
+            Console.Write("Please enter your best guess:  ");
+            string guess = Console.ReadLine();
+
+            string message = (guess == "8") ? "You Win!" : "You Lose.";
+
+            Console.WriteLine(message);
+            Console.ReadLine();
+
+
+        }
+    }
+}
